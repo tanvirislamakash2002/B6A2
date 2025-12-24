71a1a427e5d22d59eedaaa65385f87438e75f0d7
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-24 23:38:06 +0600

    added functionality to delete user data

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -20,28 +20,51 @@ const getSingleUser = async (req: Request, res: Response) => {
     try {
         const result = await userServices.getSingleUser(req.params.id as string)
 
-        if (result.rows.length===0) {
+        if (result.rows.length === 0) {
             res.status(404).json({
                 success: false,
-                message:"user not found",
+                message: "user not found",
                 data: result.rows[0]
             })
-        }else{
+        } else {
             res.status(200).json({
-                success:true,
-                message:"user fetched successfully",
+                success: true,
+                message: "user fetched successfully",
                 data: result.rows[0]
             })
         }
-    }catch(err:any){
+    } catch (err: any) {
         res.status(500).json({
-            success:false,
-            message:err.message
+            success: false,
+            message: err.message
         })
     }
 }
 
+const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const result = await userServices.deleteUser(req.params.id as string)
+        if (result.rowCount === 0) {
+            res.status(404).json({
+                success: false,
+                message: 'User not found'
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "User deleted successfully",
+                data: result.rows
+            })
+        }
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
 export const userControllers = {
     getUser,
-    getSingleUser
+    getSingleUser,
+    deleteUser
 }
