d8daed6c1f60c3fe4a7c8ad6081e94444bc4cf84
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-25 12:21:56 +0600

    created service logic for update user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -9,6 +9,13 @@ const getSingleUser = async (id: string) => {
     return result;
 }
 
+const updateUser = async (name: string, email: string, id: string) => {
+    const result = await pool.query(
+        `UPDATE users SET name=$1, email=$2 WHERE id=3 RETURNING*`, [name, email, id]
+    )
+    return result;
+}
+
 const deleteUser = async (id: string) => {
     const result = await pool.query(`DELETE FROM users WHERE id = $1`, [id])
     return result;
@@ -17,5 +24,6 @@ const deleteUser = async (id: string) => {
 export const userServices = {
     getUser,
     getSingleUser,
+    updateUser,
     deleteUser
 }
