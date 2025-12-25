4912fad810b0d2e4583e8bd1159fe778ed952f89
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-25 15:18:27 +0600

    added db table formate

--- src/config/db.ts
+++ src/config/db.ts
@@ -15,6 +15,16 @@ const initDB = async () => {
         role VARCHAR(50) NOT NULL CHECK (role IN ('admin', 'customer'))
         )
         `)
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(100) NOT NULL,
+        type VARCHAR(50) NOT NULL CHECK (type IN ('car', 'bike','van','SUV')),
+        registration_number VARCHAR(150) UNIQUE NOT NULL,
+        daily_rent_price INTEGER NOT NULL CHECK (daily_rent_price>0),
+        availability_status VARCHAR(50) NOT NULL CHECK (availability_status IN ('available', 'booked'))
+        )
+        `)
 }
 
 export default initDB;
