84588bbf4cbf00caf96aea76f9c690015363ba7b
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-26 16:45:12 +0600

    added functionality to get all bookings data

--- src/modules/bookings/bookings.controller.ts
+++ src/modules/bookings/bookings.controller.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
 import { bookingsServices } from "./booking.service";
 
-const addForBookings= async(req:Request,res:Response)=>{
+const addForBookings = async (req: Request, res: Response) => {
     const result = await bookingsServices.addForBookings(req.body)
     res.status(200).json({
-        message:'data not found',
-        data:result
+        message: 'data not found',
+        data: result
     })
 }
 
-export const bookingsControllers={
-    addForBookings
+const getAllBookings = async (req: Request, res: Response) => {
+    const result = await bookingsServices.getAllBookings();
+    res.status(200).json({
+        success: true,
+        data: result
+
+    })
+}
+
+export const bookingsControllers = {
+    addForBookings,
+    getAllBookings
 }
