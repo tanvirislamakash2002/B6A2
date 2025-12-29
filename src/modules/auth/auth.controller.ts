cb9e6f86308767cebd0e64dc98e3ab74cc8559eb
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-29 19:01:35 +0600

    added authentication functioinality

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -16,6 +16,23 @@ const createUser = async (req: Request, res: Response) => {
     }
 }
 
+const loginUser = async(req:Request, res:Response)=>{
+    const {email, password}=req.body;
+try{
+    const result = await authServices.loginUser(email, password);
+
+    res.status(200).json({
+        success:false,
+        data:result
+    })
+}catch(err:any){
+    res.status(500).json({
+        success:false,
+        message:err.message
+    })
+}
+}
+
 export const authControllers = {
     createUser,
     loginUser
