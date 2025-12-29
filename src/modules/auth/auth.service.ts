c261fee9a5823c11e74d314320ef3b3d688ec5d8
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-30 01:04:38 +0600

    seted jwt payload as user of reqest

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -24,7 +24,7 @@ const loginUser = async (email: string, password: string) => {
     if (!match) {
         return false
     }
-    const token = jwt.sign({ name: user.name, email: user.email }, config.jwtSecret as string, {
+    const token = jwt.sign({ name: user.name, email: user.email, role:user.role }, config.jwtSecret as string, {
         expiresIn: "7d"
     })
     console.log({ token });
