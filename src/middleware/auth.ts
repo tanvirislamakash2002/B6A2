e0de1749bec70b3ffa826a6f8866645486b52e54
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-29 23:36:21 +0600

    added method to decod the user from jwt

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,15 @@
 import { NextFunction, Request, Response } from "express"
+import  jwt  from "jsonwebtoken";
+import config from "../config";
 
 const auth = ()=>{
     return (req:Request, res:Response, next:NextFunction)=>{
         const token=req.headers.authorization;
-        console.log({authToken:token});
+        if(!token){
+            return res.status(500).json({message: "You are not allowed"})
+        }
+        const decoded = jwt.verify(token, config.jwtSecret as string)
+        console.log({decoded});
         next()
     }
 }
